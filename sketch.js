4eb9b6f6d44834acbbb4ac3a41e6eb9192d6e114
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-01-30 17:29:24 -0800

    added: dynamic random color generation & opacity level reduction (mouseover)

--- sketch.js
+++ sketch.js
@@ -6,6 +6,34 @@ document.addEventListener("DOMContentLoaded", function () {
   changeSizeButton.addEventListener("click", () => adjustGrid());
 });
 
+function createGrid(size) {
+  let cellCount = size * size;
+
+  for (let i = 0; i < cellCount; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("grid-cell");
+
+    let randomColor0 = Math.floor(Math.random() * 256);
+    let randomColor1 = Math.floor(Math.random() * 256);
+    let randomColor2 = Math.floor(Math.random() * 256);
+    let currentOpacity = 1;
+
+    cell.style.setProperty("--random-rgb", `169, 177, 214`);
+    cell.style.setProperty("--opacity-level", currentOpacity);
+
+    cell.addEventListener("mouseover", function () {
+      currentOpacity = Math.max(0.1, currentOpacity - 0.1);
+      cell.style.setProperty(
+        "--random-rgb",
+        `${randomColor0}, ${randomColor1}, ${randomColor2}`,
+      );
+      cell.style.setProperty("--opacity-level", currentOpacity);
+    });
+
+    container.appendChild(cell);
+  }
+}
+
 function adjustGrid() {
   let size = prompt("Enter grid size (1-100)");
 
@@ -14,31 +42,9 @@ function adjustGrid() {
     return;
   }
 
-  let cellCount = size * size;
   container.innerHTML = ""; // reset to initial state
   const setSize = 100 / size + "%";
   container.style.setProperty("--cell-size", setSize);
 
   createGrid(size);
-}
-
-function createGrid(size) {
-  let cellCount = size * size;
-
-  for (let i = 0; i < cellCount; i++) {
-    let randomColor0 = Math.floor(Math.random() * 256);
-    let randomColor1 = Math.floor(Math.random() * 256);
-    let randomColor2 = Math.floor(Math.random() * 256);
-    let currentOpacity = 1;
-
-    const cell = document.createElement("div");
-    cell.classList.add("grid-cell");
-    cell.style.setProperty(
-      "--random-rgb",
-      `${randomColor0}, ${randomColor1}, ${randomColor2}`,
-    );
-    cell.style.setProperty("--opacity-level", currentOpacity);
-    cell.addEventListener("mouseover", () => cell.classList.add("color"));
-    container.appendChild(cell);
-  }
 }
